7861a55e8195e79201ea73c56d7005a5e0e1c6ef
rafael.fonseca <150398706+raf-fonseca@users.noreply.github.com>
2024-01-11 16:28:25 -0500

    Update TTT.cpp

--- TTT.cpp
+++ TTT.cpp
@@ -3,13 +3,11 @@
 #include "fns.hpp"
 
 
-int main()
-{
-while(is_game_over() && count() <=9)
-{
-display_board();
-player_turn();
-}
-display_board();
-display_winner();
+int main(){
+  while(is_game_over() && count() <=9){
+    display_board();
+    player_turn();
+  }
+  display_board();
+  display_winner();
 }
